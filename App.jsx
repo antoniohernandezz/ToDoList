a236088726b914a170b658c240ba8e83cba2cfc1
antoniohernandezz <119897482+antoniohernandezz@users.noreply.github.com>
2023-11-10 17:26:24 -0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,30 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import { useState } from 'react';
+import { SafeAreaView } from 'react-native';
+import ToDoList from './ToDoList'; 
+import ToDoForm from './ToDoForm';
+function App() {
+
+const [tasks, setTasks] = useState([
+  'Do laundry',
+  'Go to gym',
+  'Walk dog'
+]);
+
+
+  return (
+    <SafeAreaView>
+      <ToDoList tasks ={tasks} />
+      <ToDoForm />
+    </SafeAreaView>
+  );
+}
+
+export default App;
